69964e983b3320fe5dbfd13c47d73e81d3df286a
topaz11100 <topaz11100@gmail.com>
2025-07-30 02:13:44 +0900

    1

---
+++ script.js
@@ -0,0 +1,35 @@
+const ser_btn = document.getElementById("serbtn")
+ser_btn.addEventListener('click', ser_btn_click);
+const ser_txt = document.getElementById("serial_process_text")
+
+const model_btn = document.getElementById("mdlbtn")
+model_btn.addEventListener('click', model_btn_click);
+const model_txt = document.getElementById("model_process_text")
+
+const result_txt = document.getElementById("result")
+
+const infer_div = document.getElementById("inference")
+
+async function ser_btn_click()
+{
+    
+}
+
+async function model_btn_click()
+{
+
+}
+
+async function model_load(url)
+{
+
+}
+
+const txtencoder = new TextEncoder();
+
+async function page_init()
+{
+    
+}
+
+page_init();
