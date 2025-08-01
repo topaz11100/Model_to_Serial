a3ad144a52254d7e1ebcef072fed22c3f8331fcb
topaz11100 <topaz11100@gmail.com>
2025-08-01 15:31:37 +0900

    1

---
+++ arduino_test_code/arduino_test_code.ino
@@ -0,0 +1,26 @@
+#include <IO_helper.hpp>
+#include <base_part.hpp>
+
+Actuator led(13);
+
+void setup()
+{
+    Serial.begin(9600);
+    led.init();
+}
+
+void loop()
+{
+    char r = Rx::r_char('?');
+    switch(r)
+    {
+        case '1':
+            led.write(true);
+            break;
+        case '0':
+            led.write(false);
+            break;
+        default:
+            break;
+    }
+}
